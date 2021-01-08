3693fdada8b179c0e114b62047a87e3455dc41b4
antalszava <antalszava@gmail.com>
2021-01-08 16:24:37 -0500

    Draft

--- track_bugs.py
+++ track_bugs.py
@@ -22,15 +22,12 @@ def get_bugs_for_repo(github_org = 'pennylaneai'):
     
     return bug_db
 
-def track_bugs():
+headers = ["#", "Date", "Date replied", "Channel Type", "Type", "Component", "Brief description"]
 
-    # 1. Get the bugs for an organization
-    github_org = 'pennylaneai'
-    bug_db = get_bugs_for_repo(github_org)
+def create_csv(bug_db):
 
-    # 2. Create the data fit for the csv files
+    # 1. Create the data fit for the csv files
     data = []
-    csv_headers = ["#", "Date", "Date replied", "Channel Type", "Type", "Component", "Brief description"]
 
     for repo_name, bugs in bug_db:
         for bug in bugs:
@@ -53,10 +50,50 @@ def track_bugs():
 
             data.append(desc)
 
-    # 3. Write csv file
+    # 2. Write csv file
     with open('bugs.csv', 'w', newline='') as file:
         writer = csv.writer(file)
         writer.writerows(data)
 
+def get_markdown(bug_db):
+
+    # 1. Create the markdown string
+    data = []
+
+    for repo_name, bugs in bug_db:
+        for bug in bugs:
+            base_url = f'https://github.com/{github_org}/{repo_name}/issues/'
+
+            bug_number_in_list = ""
+            date = bug['created_at'].split('T')[0].replace('-','/')
+            date_replied = ""
+            channel_type = "GitHub"
+            entry_type = "Bug"
+            component = repo_name
+            name = bug['title']
+
+            num = bug['url'].split('/')[-1]
+            url = base_url + num
+
+            brief_desc = f'=HYPERLINK("{url}", "{name}")'
+
+            desc = [bug_number_in_list, date, date_replied, channel_type, entry_type, component, brief_desc]
+
+            data.append(desc)
+
+
+def need_new_readme(markdown):
+
+def update_readme():
+    pass
+    # 1. check if need update
+    # 2. update
+
 if __name__ == "__main__":
-    track_bugs()
+
+    bug_db = get_bugs_for_repo()
+    create_csv(bug_db)
+    markdown = get_markdown(bug_db)
+
+    if need_new_readme(markdown):
+        update_readme(bug_db)
